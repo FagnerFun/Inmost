f3fe4b4fbd896d1c53efc4c8843378b8e7058d0c
Fagner Fun <fagner@fmuniz.com.br>
2015-10-13 23:22:12 -0300

    convidar + inicio threadNotificacao

---
+++ Projeto_MVC/InMost/Controllers/ThreadNotificacaoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InMost.Controllers
+{
+    public class ThreadNotificacaoController : Controller
+    {
+
+        public static Thread threadNotificacao { get; set; }
+
+        public void IniciarThread()
+        {
+            ThreadNotificacaoController.threadNotificacao = new Thread(new ThreadStart(AtualizarNotificacoes));
+            ThreadNotificacaoController.threadNotificacao.Start();
+        }
+
+        private void AtualizarNotificacoes()
+        {
+
+        }
+    }
+}
---
+++ Projeto_MVC/InMost/Entity/NotificacaoEntity.cs
@@ -0,0 +1,14 @@
+﻿using InMost.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InMost.Entity
+{
+    class NotificacaoEntity
+    {
+        public Membros emissor { get; set; }
+    }
+}
